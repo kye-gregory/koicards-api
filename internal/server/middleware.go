d5e31b9b32e09ae2d85b1c5e2794099cfbe64d68
kye-gregory <kyegregory002@gmail.com>
2024-11-03 18:09:30 +1100

    feat: add simple auth middleware

--- internal/server/middleware.go
+++ internal/server/middleware.go
@@ -12,3 +12,15 @@ func RequestLoggerMiddleware(next http.Handler) http.HandlerFunc {
 		next.ServeHTTP(w,r)
 	}
 }
+
+func RequireAuthMiddleware(next http.Handler) http.HandlerFunc {
+	return func (w http.ResponseWriter, r *http.Request) {
+		token := r.Header.Get("Authorization")
+		if token != "ValidAuthToken" {
+			http.Error(w, "Unauthorized", http.StatusUnauthorized)
+			return
+		}
+
+		next.ServeHTTP(w,r)
+	}
+}
