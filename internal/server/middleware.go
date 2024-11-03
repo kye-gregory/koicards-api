65dc3a28fd94a8858bb4fd7b613ae97c62c7a96a
kye-gregory <kyegregory002@gmail.com>
2024-11-03 18:10:34 +1100

    feat: add middleware chain func

--- internal/server/middleware.go
+++ internal/server/middleware.go
@@ -5,6 +5,17 @@ import (
 	"net/http"
 )
 
+type Middleware func(http.Handler) http.HandlerFunc
+
+func MiddlewareChain(middlewares ... Middleware) Middleware {
+	return func (next http.Handler) http.HandlerFunc  {
+		for i := len(middlewares) - 1; i >= 0; i-- {
+			next = middlewares[i](next)
+		}
+
+		return next.ServeHTTP
+	}
+}
 
 func RequestLoggerMiddleware(next http.Handler) http.HandlerFunc {
 	return func (w http.ResponseWriter, r *http.Request) {
