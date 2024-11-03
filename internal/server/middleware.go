8d7586d927b0376f7595742db62432dfdbf5b1fc
kye-gregory <kyegregory002@gmail.com>
2024-11-03 18:09:05 +1100

    feat: add simple logging middleware

---
+++ internal/server/middleware.go
@@ -0,0 +1,14 @@
+package server
+
+import (
+	"log"
+	"net/http"
+)
+
+
+func RequestLoggerMiddleware(next http.Handler) http.HandlerFunc {
+	return func (w http.ResponseWriter, r *http.Request) {
+		log.Printf("Method %s, path: %s", r.Method, r.URL.Path)
+		next.ServeHTTP(w,r)
+	}
+}
