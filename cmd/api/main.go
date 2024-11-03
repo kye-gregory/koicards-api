d52665752946d416268f4d3626e212226c5ccfc5
kye-gregory <kyegregory002@gmail.com>
2024-11-03 17:28:03 +1100

    feat: create and run server in main.go

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -5,9 +5,15 @@ import (
 	"errors"
 	"fmt"
 	"io"
+	"log"
+	"net"
+	"net/http"
 	"os"
 	"os/signal"
+	"sync"
 	"time"
+
+	"github.com/kye-gregory/koicards-api/internal/server"
 )
 
 func run(
@@ -18,29 +24,52 @@ func run(
 	stdout 	io.Writer,
 	stderr 	io.Writer,
 ) error {
+	log.SetOutput(stderr)
+	
 	// Watch System Interrupt
 	ctx, cancel := signal.NotifyContext(ctx, os.Interrupt)
     defer cancel()
 
-	// Mock Execution in GoRoutine
+	// Create The Server
+	handler := server.NewServer()
+	httpServer := &http.Server{
+		Addr:    net.JoinHostPort("localhost", "8080"),
+		Handler: handler,
+	}
+
+	// Run Server
 	go func() {
-		for {
-			select {
-			case <-ctx.Done():
-				return
-			default:
-				fmt.Fprintln(stdout, "Working...")
-				time.Sleep(1 * time.Second)
-			}
+		log.Println("Listening for requests on", httpServer.Addr)
+		if err := httpServer.ListenAndServe(); err != nil && err != http.ErrServerClosed {
+			log.Printf("ListenAndServe error: %v", err)
+			cancel()
 		}
 	}()
 
-	// Wait For GoRoutine
-	<- ctx.Done()
+	// Finalize Server
+	var wg sync.WaitGroup
+	wg.Add(1)
+	go func() {
+		defer wg.Done()
 
-	// Catches System Interrupt
-    if ctx.Err() == nil { return nil }
-	return errors.New("operation interrupted by system signal")
+		// Catch System Interrupt
+		<-ctx.Done()
+
+		// Add Shutdown Timeout
+		shutdownCtx := context.Background()
+		shutdownCtx, cancel := context.WithTimeout(shutdownCtx, 10 * time.Second)
+		defer cancel()
+
+		// Shutdown Server
+		log.Println("Server shutting down...")
+		if err := httpServer.Shutdown(shutdownCtx); err != nil {
+			log.Printf("error shutting down http server: %s", err)
+		}
+	}()
+
+	// Exit Run
+	wg.Wait()
+	return nil
 }
 
 
