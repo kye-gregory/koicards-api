9dc9ae76b0993b30ffd89faa0c6d4d7f75ca83ba
kye-gregory <kyegregory002@gmail.com>
2024-11-03 17:35:00 +1100

    feat: add errors module for extra custom struct

---
+++ pkg/errors/errors.go
@@ -0,0 +1,31 @@
+package errors
+
+import (
+	"fmt"
+	"time"
+)
+
+// ErrorStack collects multiple errors and implements the error interface.
+type ErrorStack struct {
+	Errors []error
+}
+
+// Appends an error to the ErrorStack.
+func (m *ErrorStack) Add(err error) {
+	if err != nil {
+		timestampedErr := fmt.Errorf("%s: %w", time.Now().Format(time.RFC3339), err)
+		m.Errors = append(m.Errors, timestampedErr)
+	}
+}
+
+// Implements the error interface for ErrorStack.
+func (m *ErrorStack) Error() string {
+	if len(m.Errors) == 0 {
+		return "no errors"
+	}
+	msg := "multiple errors:"
+	for _, err := range m.Errors {
+		msg += "\n - " + err.Error()
+	}
+	return msg
+}
