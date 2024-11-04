77af3624a918d8269ed46faefa665379da9990fe
kye-gregory <kyegregory002@gmail.com>
2024-11-04 21:32:08 +1100

    feat: add general purpose error package

---
+++ pkg/errors/stack.go
@@ -0,0 +1,39 @@
+package errors
+
+// Stack collects multiple errors and implements the error interface.
+type Stack struct {
+	Errors []error
+}
+
+// Creates a new Stack
+func NewStack() *Stack {
+	s := new(Stack)
+	return s
+}
+
+// Appends an error to the Stack.
+func (s *Stack) Add(err error) {
+	if err != nil {
+		s.Errors = append(s.Errors, err)
+	}
+}
+
+// Implements the error interface.
+func (s *Stack) Error() string {
+	// No Errors
+	if len(s.Errors) == 0 { return "" }
+
+	// Construct Err String
+	msg := "\nErrors:"
+	for _, err := range s.Errors {
+		msg += "\n - " + err.Error()
+	}
+
+	// Return
+	return msg
+}
+
+// Resets the Stack
+func (s *Stack) Clear() {
+	s.Errors = nil
+}
