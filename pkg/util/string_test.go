e4919493ba37889612b49bd57268e28f3c14840c
Kye Gregory <kyegregory002@gmail.com>
2024-11-25 18:18:19 +1100

    feat: fixed GenerateRandomString() variance test

--- pkg/util/string_test.go
+++ pkg/util/string_test.go
@@ -13,6 +13,7 @@ func TestGenerateRandomString(t *testing.T) {
 		maxLen   	int
 		charSet	 	string
 	}{
+		{1, 1, "a"},
 		{-5, 10, ""},
 		{5, 0, "abc"},
 		{0, 1, "123"},
@@ -22,32 +23,35 @@ func TestGenerateRandomString(t *testing.T) {
 
 	for _, test := range tests {
 		// Generate Strings
-		resultA := GenerateRandomString(test.minLen, test.maxLen, test.charSet)
-		resultB := GenerateRandomString(test.minLen, test.maxLen, test.charSet)
+		result := GenerateRandomString(test.minLen, test.maxLen, test.charSet)
 
 		// Test Length
-		validLengthA := validate.MinLength(resultA, test.minLen) && (validate.MaxLength(resultA, test.maxLen) || test.maxLen < test.minLen)
-		validLengthB := validate.MinLength(resultB, test.minLen) && (validate.MaxLength(resultB, test.maxLen) || test.maxLen < test.minLen)
+		validLengthA := validate.MinMaxLength(result, test.minLen, test.maxLen) || test.maxLen < test.minLen
 		if !validLengthA {
-			t.Errorf("GenerateRandomString(%d, %d, %q) = %v; invalid length.", test.minLen, test.maxLen, test.charSet, resultA)
-		}
-		if !validLengthB {
-			t.Errorf("GenerateRandomString(%d, %d, %q) = %v; invalid length.", test.minLen, test.maxLen, test.charSet, resultB)
+			t.Errorf("GenerateRandomString(%d, %d, %q) = %v; invalid length.", test.minLen, test.maxLen, test.charSet, result)
 		}
 
 		// Test Characters
-		validCharsA := validate.OnlyContainsRunes(resultA, test.charSet)
-		validCharsB := validate.OnlyContainsRunes(resultB, test.charSet)
+		validCharsA := validate.OnlyContainsRunes(result, test.charSet)
 		if !validCharsA {
-			t.Errorf("GenerateRandomString(%d, %d, %q) = %v; invalid characters.", test.minLen, test.maxLen, test.charSet, resultA)
+			t.Errorf("GenerateRandomString(%d, %d, %q) = %v; invalid characters.", test.minLen, test.maxLen, test.charSet, result)
 		}
-		if !validCharsB {
-			t.Errorf("GenerateRandomString(%d, %d, %q) = %v; invalid characters.", test.minLen, test.maxLen, test.charSet, resultB)
+	}
+
+	// Test Variability
+	results := []string{}
+	charset := "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789~`!@#$%^&*()-_+=[]{}<>,.'\\/\":; \n\té漢🙂😀😃😄"
+	for i := 0; i < 100; i++ {
+		result := GenerateRandomString(10, 10, charset)
+		isDuplicate := false
+		for _, ele := range results {
+			if ele == result { isDuplicate = true; break }
 		}
 
-		// Test Variability
-		if resultA == resultB && validate.MinLength(test.charSet, 1) {
-			t.Errorf("GenerateRandomString(%d, %d, %q); A = %v; B = %v; duplicate results.", test.minLen, test.maxLen, test.charSet, resultA, resultB)
-		}
+		if (!isDuplicate) { results = append(results, result) }
+	}
+
+	if len(results) < 95 {
+		t.Errorf("GenerateRandomString() duplicates out of 100 = %v; invalid varience.", 100 - len(results))
 	}
 }
