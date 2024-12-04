8a9b97e6c3a492ccfcd7dd83afd4306e5d97c6de
Kye Gregory <kyegregory002@gmail.com>
2024-12-04 16:43:20 +1100

    update user struct to match database

--- internal/models/user.go
+++ internal/models/user.go
@@ -1,19 +1,24 @@
 package models
 
 import (
+	"time"
+
 	userVO "github.com/kye-gregory/koicards-api/internal/valueobjects/user"
 )
 
 type User struct {
-	ID			int		`json:"id"`
+	ID			int				`json:"id"`
 	Email 		userVO.Email	`json:"email"`
 	Username 	userVO.Username	`json:"username"`
 	Password 	userVO.Password	`json:"-"`
 	IsVerified	bool			`json:"isVerified"`
+	CreatedAt	time.Time		`json:"createdAt"`
+	Status		string			`json:"status"`
 }
 
 func NewUser(email userVO.Email, username userVO.Username, password userVO.Password) *User{
 	user := User {
+		ID:			-1,
 		Email: 		email,
 		Username: 	username,
 		Password: 	password,
--- internal/store/postgres/migrations/20241202094530_create_users_table.sql
+++ internal/store/postgres/migrations/20241202094530_create_users_table.sql
@@ -4,9 +4,9 @@ CREATE TYPE account_status_enum AS ENUM ('active', 'inactive', 'suspended');
 
 CREATE TABLE user_account (
     id SERIAL PRIMARY KEY,
-    username VARCHAR(24) UNIQUE NOT NULL,
-    password_hash TEXT NOT NULL,
     email VARCHAR(255) UNIQUE NOT NULL,
+    username VARCHAR(24) UNIQUE NOT NULL,
+    password_hash TEXT NOT NULL,  
     is_email_verified BOOLEAN DEFAULT FALSE,
     created_at TIMESTAMPTZ DEFAULT CURRENT_TIMESTAMP,
     account_status account_status_enum DEFAULT 'active'
