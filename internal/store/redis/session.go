a6dbc9f6a053e7be5a01263f1d52c1047d94c695
Kye Gregory <kyegregory002@gmail.com>
2024-12-05 10:55:10 +1100

    feat: implement redis session store

--- internal/store/redis/session.go
+++ internal/store/redis/session.go
@@ -1,7 +1,8 @@
 package redis
 
 import (
-	"fmt"
+	"encoding/json"
+	"time"
 
 	"github.com/go-redis/redis"
 	"github.com/kye-gregory/koicards-api/internal/models"
@@ -15,6 +16,34 @@ func NewSessionStore(db *redis.Client) *SessionStore {
 	return &SessionStore{db: db}
 }
 
-func (store *SessionStore) CreateSession(userID int) (*models.Session, error) {return nil, fmt.Errorf("Not Implemented")}
-func (store *SessionStore) DeleteSession(sessionID string) error {return fmt.Errorf("Not Implemented")}
-func (store *SessionStore) VerifySession(sessionID string) (bool, error) {return false, fmt.Errorf("Not Implemented")}
+func (store *SessionStore) CreateSession(session *models.Session) error {
+	// Marshal Data
+	data, err := json.Marshal(session.Data)
+    if err != nil {  return err  }
+
+	// Update DB
+	// key := "user_sessions:" + (string)(session.Data.UserID)
+	err = store.db.Set("session:"+session.ID, data, time.Duration(session.ExpiryInNS)).Err()
+	if err != nil {  return err  }
+
+	return nil
+}
+
+
+func (store *SessionStore) DeleteSession(sessionID string) error {
+	return store.db.Del(sessionID).Err()
+}
+
+
+func (store *SessionStore) GetSessionData(sessionID string) (*models.SessionData, error) {
+	// Get Data as String
+	val, err := store.db.Get("session:"+sessionID).Result()
+    if err != nil { return nil, err }
+
+	// Marshal Data To Struct
+    var data models.SessionData
+    err = json.Unmarshal([]byte(val), &data)
+    if err != nil { return nil, err }
+
+	return &data, nil
+}
